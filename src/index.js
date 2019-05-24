3a6e604842d05e989381c626209cc2ba77766006
SalathielGenese <salathielgenese@gmail.com>
2019-05-24 15:07:18 +0100

    Support logging

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require( 'express' );
+const debug = require( 'debug' );
 
 
 
+const NS = 'cloud-run-102:src/index:debug';
 const PORT = process.env.PORT || 3000;
 
 express().use( ( request, response ) =>
 {
-    response.json({
+    const body = {
         url: request.url,
         query: request.query,
         timestamp: Date.now(),
         protocol: request.protocol,
-    });
-}).listen( PORT );
+    };
+
+    debug( NS )( body );
+    response.json( body );
+}).listen( PORT, () =>
+{
+    debug( NS )( `Started on port :${ PORT }` );
+});
